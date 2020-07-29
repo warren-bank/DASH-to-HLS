33603158b7f08f268dd43a1f819f2e2295dde01e
Warren R Bank <warren.r.bank@gmail.com>
2020-07-29 15:35:22 -0700

    update demo: add selection of sender CDN hosts

--- demos/02-sender/cast_video.js
+++ demos/02-sender/cast_video.js
@@ -3,6 +3,8 @@
   const $port    = document.getElementById('port')
   const $tls     = document.getElementById('tls')
 
+  const $sender  = document.getElementById('sender_cdn')
+
   const $samples = document.getElementById('sample_videos')
   const $url     = document.getElementById('url')
   const $vod     = document.getElementById('vod')
@@ -25,14 +27,16 @@
     const port = $port.value || $port.getAttribute('placeholder')
     const server_url = `http${$tls.checked ? 's' : ''}://${host}:${port}/`
 
+    const sender = $sender.value || $sender.querySelector(':scope > option[value^="http:"]').value
+
     let url
-    url = btoa($url.value)                                                               // base64 encode URL to DASH manifest
-    url = server_url + url                                                               // construct URL to read HLS master manifest
+    url = btoa($url.value)                   // base64 encode URL to DASH manifest
+    url = server_url + url                   // construct URL to read HLS master manifest
     if (vod.checked)
-      url = url + '?VOD=1'                                                               // append a querystring parameter if VOD is checked
-    url = url + '#master.m3u8'                                                           // append an HLS file extension to the URL hash (as a hint to the video player)
-    url = btoa(url)                                                                      // base64 encode URL to  HLS master manifest
-    url = 'http://webcast-reloaded.surge.sh/index.html' + '#/watch/' + url               // construct URL to view HLS master manifest in an HTML5 video player
+      url = url + '?VOD=1'                   // append a querystring parameter if VOD is checked
+    url = url + '#master.m3u8'               // append an HLS file extension to the URL hash (as a hint to the video player)
+    url = btoa(url)                          // base64 encode URL to  HLS master manifest
+    url = sender + '#/watch/' + url          // construct URL to view HLS master manifest in an HTML5 video player
 
     window.location = url
   }
