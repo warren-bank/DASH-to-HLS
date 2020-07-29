04169ce98f3f1fd7bca56355362fe33ab04c791f
Warren R Bank <warren.r.bank@gmail.com>
2020-07-29 16:01:58 -0700

    update demo: use cookies to persist config values

--- demos/02-sender/cast_video.js
+++ demos/02-sender/cast_video.js
@@ -10,6 +10,39 @@
   const $vod     = document.getElementById('vod')
   const $button  = document.querySelector('button')
 
+  // ---------------------------------------------------------------------------
+
+  const get_cookie_value = function(key) {
+    const pattern = new RegExp('(?:^|;)\\s*' + key + '\\s*=\\s*([^;]+)(?:;|$)')
+    const cookies = document.cookie
+    const matches = pattern.exec(cookies)
+    return (matches && matches.length) ? matches[1] : ''
+  }
+
+  const set_cookie_value = function(key, val) {
+    document.cookie = `${key}=${val};domain=${window.location.hostname};path=${window.location.pathname};max-age=${60*60*24*365}`
+  }
+
+  const cookie_name_prefix = 'dash_to_hls_demo_02sender_'
+
+  const prepopulate_form_fields = function() {
+    const host = get_cookie_value(cookie_name_prefix + 'host')
+    const port = get_cookie_value(cookie_name_prefix + 'port')
+    const tls  = get_cookie_value(cookie_name_prefix + 'tls')
+
+    if (host) $host.value = host
+    if (port) $port.value = port
+    if (tls === '1') $tls.checked = true
+  }
+
+  const persist_form_fields = function (host, port, tls) {
+    set_cookie_value(cookie_name_prefix + 'host', host)
+    set_cookie_value(cookie_name_prefix + 'port', port)
+    set_cookie_value(cookie_name_prefix + 'tls',  (tls ? '1' : '0'))
+  }
+
+  // ---------------------------------------------------------------------------
+
   const load_sample = function(event) {
     event.stopPropagation()
     event.preventDefault()
@@ -25,7 +58,9 @@
 
     const host = $host.value || $host.getAttribute('placeholder')
     const port = $port.value || $port.getAttribute('placeholder')
-    const server_url = `http${$tls.checked ? 's' : ''}://${host}:${port}/`
+    const tls  = $tls.checked
+
+    const server_url = `http${tls ? 's' : ''}://${host}:${port}/`
 
     const sender = $sender.value || $sender.querySelector(':scope > option[value^="http:"]').value
 
@@ -38,8 +73,14 @@
     url = btoa(url)                          // base64 encode URL to  HLS master manifest
     url = sender + '#/watch/' + url          // construct URL to view HLS master manifest in an HTML5 video player
 
+    persist_form_fields(host, port, tls)
+
     window.location = url
   }
+
+  // ---------------------------------------------------------------------------
+
+  prepopulate_form_fields()
 
   $samples.addEventListener('change', load_sample)
   $button.addEventListener( 'click',  redirect_page)
